c7f580a417973574a258f26b1c4be1a33396f562
omarmabbasi <omarmabbasi@gmail.com>
2019-06-20 17:29:45 -0700

    Initi webpack

---
+++ jquerylite/src/index.js
@@ -0,0 +1,17 @@
+
+
+
+function $l(selector){
+  // debugger;
+  if(typeof selector === "string") {
+    querySelector = document.querySelectorAll(selector);
+    selectorArray = Array.from(querySelector); 
+    return selectorDomNode = new DOMNodeCollection(selectorArray)
+  } else if(selector instanceof HTMLElement) {
+    return selectorDOMNode = new DOMNodeCollection([selector])
+  }
+
+}
+
+window.$l = $l;
+
